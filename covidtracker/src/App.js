7f4d40460d9b71042eaa0c4e34ec632884c8f2c5
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2023-01-12 13:17:59 +0100

    state

--- covidtracker/src/App.js
+++ covidtracker/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios';
 
 function App() {
   return (
