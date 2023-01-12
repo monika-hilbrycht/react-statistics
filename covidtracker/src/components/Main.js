73470f898eaeae8324fccb2c00177329b8e9265f
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2023-01-12 12:58:20 +0100

    fetch api

--- covidtracker/src/components/Main.js
+++ covidtracker/src/components/Main.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
+import axios from 'axios';
 
 export default function Main() {
+
+    useEffect(() => {
+
+        axios.get('https://api.covid19api.com/summary').then(res=>{
+            console.log(res.data)
+        }).catch(err=>{console.log(err)})
+
+    }, [])
+
+
     return (
         <div>
             <h1>CovidStats</h1>
